242f225d03c53b0f26843dcb4a512566185435ce
abdellani <abdellani@gmail.com>
2019-10-14 23:47:16 +0100

    Add left-side panel

--- src/imdb/components/leftPanel.js
+++ src/imdb/components/leftPanel.js
@@ -1,3 +1,63 @@
 import React from "react"
+import it from "../images/it-chapter.webp"
+import theLionKing from "../images/theLionKing.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
-cons
+const LeftPanel = () =>
+  <div id="left-panel">
+    <div
+      className="w-100 px-1 py-1"
+    >
+      <img className="w-100" src={it} />
+    </div>
+    <div
+      className="titles d-flex flex-column "
+    >
+      <div
+        className="d-flex justify-content-between px-3 py-3 font-weight-bold"
+      >
+        opening this week
+        <FontAwesomeIcon icon={faPlus} />
+      </div>
+      <div
+        className="d-flex justify-content-between px-3 py-3 font-weight-bold selected"
+      >
+        now playing
+        <FontAwesomeIcon icon={faMinus} />
+      </div>
+      <div class="sub-list">
+        <div  className="px-3 d-flex flex-row justify-content-between align-items-center">
+          <div className="d-flex align-items-center">
+            <div>
+            <img src={theLionKing}/>
+            </div>
+            <div className="pl-2 d-flex flex-column">
+              <div className="font-weight-bold">
+                the lion king
+              </div>
+              <div className="text-secondary">
+                5534.7M
+              </div>
+            </div>
+          </div>
+          <div className="text-warning font-weight-bold">
+            7.8
+          </div>
+        </div>
+      </div>
+      <div
+        className="d-flex justify-content-between py-3 px-3 font-weight-bold"
+      >
+        comming soon
+        <FontAwesomeIcon icon={faPlus} />
+      </div>
+      <div
+        className="d-flex justify-content-between py-3 px-3 font-weight-bold"
+      >
+        around the web
+        <FontAwesomeIcon icon={faPlus} />
+      </div>
+    </div>
+  </div>
+export default LeftPanel;
--- src/imdb/index.js
+++ src/imdb/index.js
@@ -1,23 +1,30 @@
 import React from "react"
 import "./scss/index.scss"
-import RightPanel from "./components/rightPanel"
 import Navbar from "./components/navbar"
+import LeftPanel from "./components/leftPanel"
+import RightPanel from "./components/rightPanel"
 
-const Application = ()=>
-<div className="container bg-white my-5 ">
- <div className="row">
-  <div  className="col-md-9 container">
+const Application = () =>
+  <div className="container bg-white my-5 ">
     <div className="row">
-      <div className="col-md-12">
-    <Navbar/>
-    </div>
+      <div className="col-md-9 container">
+        <div className="row">
+          <div className="col-md-12">
+            <Navbar />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <LeftPanel/>
+          </div>
+
+        </div>
+      </div>
+      <div className="col-md-3 px-0">
+        <RightPanel />
+      </div>
     </div>
   </div>
-  <div className="col-md-3 px-0">
-    <RightPanel/>
-  </div>
- </div>
-</div>
 
 
- export default Application
+export default Application
