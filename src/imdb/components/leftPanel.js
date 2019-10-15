ca24549ba612276f348d7cf42d2fa2d7197b57cf
abdellani <abdellani@gmail.com>
2019-10-15 20:16:43 +0100

    finish imdb

--- src/imdb/components/leftPanel.js
+++ src/imdb/components/leftPanel.js
@@ -1,10 +1,14 @@
 import React from "react"
 import { Fragment } from "react"
-import it from "../images/it-chapter.webp"
-import theLionKing from "../images/theLionKing.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
+import it from "../images/it-chapter.webp"
+import theLionKing from "../images/theLionKing.png"
+import itChapter from "../images/itChapter.png"
+import angelHasFallen from "../images/angelHasFallen.png"
+import hustlers from "../images/hustlers.png"
+import downtonAbbey from "../images/downtonAbbey.png"
 
 const Item = ({ content }) =>
   <div
@@ -14,7 +18,7 @@ const Item = ({ content }) =>
     <FontAwesomeIcon icon={faPlus} />
   </div>
 
-const SubMenuItem = ({image,title,movieGoers,rating}) =>
+const SubMenuItem = ({ image, title, benefits, rating }) =>
   <div className="px-3 py-1 d-flex flex-row justify-content-between align-items-center">
     <div className="d-flex align-items-center">
       <div>
@@ -25,7 +29,7 @@ const SubMenuItem = ({image,title,movieGoers,rating}) =>
           {title}
         </div>
         <div className="text-secondary">
-          {movieGoers}
+          {benefits}
         </div>
       </div>
     </div>
@@ -43,42 +47,39 @@ const SelectedItem = ({ content }) =>
       <FontAwesomeIcon icon={faMinus} />
     </div>
     <div class="sub-list">
-    <SubMenuItem 
-    image={ theLionKing} 
-    title={"the lion king"}
-    movieGoers={"5557M"}
-    rating={"7.8"}
-    />
-    <SubMenuItem 
-    image={ theLionKing} 
-    title={"the lion king"}
-    movieGoers={"5557M"}
-    rating={"7.8"}
-    />
-    <SubMenuItem 
-    image={ theLionKing} 
-    title={"the lion king"}
-    movieGoers={"5557M"}
-    rating={"7.8"}
-    />
-    <SubMenuItem 
-    image={ theLionKing} 
-    title={"the lion king"}
-    movieGoers={"5557M"}
-    rating={"7.8"}
-    />
-    <SubMenuItem 
-    image={ theLionKing} 
-    title={"the lion king"}
-    movieGoers={"5557M"}
-    rating={"7.8"}
-    />
-    <SubMenuItem 
-    image={ theLionKing} 
-    title={"the lion king"}
-    movieGoers={"5557M"}
-    rating={"7.8"}
-    />
+      <SubMenuItem
+        image={theLionKing}
+        title={"the lion king"}
+        benefits={"$537.7M"}
+        rating={"7.8"}
+      />
+      <SubMenuItem
+        image={itChapter}
+        title={"It Chapter Two"}
+        benefits={"$17.0M"}
+        rating={"7.0"}
+      />
+      <SubMenuItem
+        image={angelHasFallen}
+        title={"Angel has fallen"}
+        benefits={"$64.7M"}
+        rating={"7.1"}
+      />
+      <SubMenuItem
+        image={hustlers}
+        title={"Hustlers"}
+        benefits={"$16.8M"}
+        rating={"6.8"}
+      />
+      <SubMenuItem
+        image={downtonAbbey}
+        title={"Downton Abbey"}
+        benefits={"$31.0M"}
+        rating={"7.8"}
+      />
+      <div className="more-result text-center py-3">
+        More results
+      </div>
     </div>
   </Fragment>
 
--- src/imdb/components/main.js
+++ src/imdb/components/main.js
@@ -1,9 +1,13 @@
 import React from "react"
 import { Fragment } from "react"
-import greenBook from "../images/greenbook.jpeg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHandPointRight } from "@fortawesome/free-regular-svg-icons"
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons"
+import greenBook from "../images/greenbook.jpeg"
+import theLionKing from "../images/theLionKing.jpeg"
+import angelHasFallen from "../images/angelHasFallen.jpg"
+import terminator from "../images/terminator.jpg"
+import abominable from "../images/abominable.png"
 
 let Main = () =>
   <Fragment>
@@ -22,13 +26,13 @@ let Main = () =>
           <span className="red-circle mx-2 d-flex align-items-center justify-content-center">5</span>
           <div>
             All filter
-                  </div>
+          </div>
         </div>
       </div>
     </div>
     <div className="row explore-movie py-3">
       <div className="col-md-4 px-1">
-        <img src={greenBook} />
+        <img src={theLionKing} />
       </div>
       <div className="col-md-3 px-1">
         <div className="second-column d-flex flex-column">
@@ -65,19 +69,19 @@ let Main = () =>
         </div>
       </div>
       <div className="col-md-3 px-1">
-        <img src={greenBook} />
+        <img src={angelHasFallen} />
       </div>
       <div className="col-md-2 px-1">
-      <div className="fourth-column d-flex flex-column">
+        <div className="fourth-column d-flex flex-column">
           <div
             className="d-flex justify-content-center align-items-center text-dark font-weight-bold mb-1"
           >
-                VIEW ALL
+            VIEW ALL
           </div>
           <div
-            className="d-flex justify-content-center align-items-center text-dark font-weight-bold"
+            className="yellow-imdb d-flex justify-content-center align-items-center text-dark"
           >
-                IMDB
+            IMDB
           </div>
         </div>
       </div>
@@ -92,10 +96,10 @@ let Main = () =>
         <img src={greenBook} />
       </div>
       <div className="col-md-4">
-        <img src={greenBook} />
+        <img src={terminator} />
       </div>
       <div className="col-md-4">
-        <img src={greenBook} />
+        <img src={abominable} />
       </div>
     </div>
   </Fragment>
